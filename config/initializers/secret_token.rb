6b03443f449e9682d7ce9252efbfae9e3f3e8749
sinsoku <sinsoku.listy@gmail.com>
2013-10-02 17:42:28 +0900

    rails new suicat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Suicat::Application.config.secret_key_base = 'e1cf83dd4fc97f36c357727326fc8e37d48faad4fe804b3016398fbca64c8a8e0cba14a719dd9c74e23155eec8d12f705347be336cbaba7a240485424440dc81'
