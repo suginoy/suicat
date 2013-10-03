953952dbdb902d813516d9251ff9ddaaebbef6e0
sinsoku <sinsoku.listy@gmail.com>
2013-10-03 16:37:36 +0900

    ユーザ作成時にapi keyが付与されるように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   class << self
     def find_for_twitter_oauth(auth, signed_in_resource=nil)
       User.where(provider: auth.provider, uid: auth.uid)
-        .first_or_create(name: auth.info.nickname)
+        .first_or_create(name: auth.info.nickname, key: Devise.friendly_token)
     end
   end
 end
