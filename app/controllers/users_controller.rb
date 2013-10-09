a787335cb49b26fad3a035c11e2b9662f844262d
sinsoku <sinsoku.listy@gmail.com>
2013-10-09 17:38:23 +0900

    routingを調整し、簡易のviewを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def update
+    # TODO:
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Suicat::Application.routes.draw do
-  root to: 'home#index'
+  resources :users, only: :update
+  resources :histories, only: :index
+
+  namespace :api do
+    get 'users/:user_id/histories', to: 'users/histories#index', as: 'user_histories'
+    post 'users/:user_id/histories', to: 'users/histories#create', as: nil
+  end
+
   devise_scope :user do
     delete 'logout', to: 'devise/sessions#destroy'
   end
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  namespace :api do
-    post 'users/:user_id/histories', to: 'users/histories#create', as: 'user_histories'
+  authenticated :user do
+    root to: 'histories#index', as: :user_root
   end
+  root to: 'home#index'
 end
