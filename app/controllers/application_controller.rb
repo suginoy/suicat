0a0a0044294554924880c36128c46f95f37e01c9
sinsoku <sinsoku.listy@gmail.com>
2013-10-16 17:58:01 +0900

    iphoneの簡易viewを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_iphone_format
+
+  private
+  def set_iphone_format
+    request.formats.insert(0, Mime::Type.new('text/html', :iphone)) if iphone_request?
+  end
+
+  def iphone_request?
+    request.user_agent =~ /(Mobile.+Safari)/
+  end
 end
